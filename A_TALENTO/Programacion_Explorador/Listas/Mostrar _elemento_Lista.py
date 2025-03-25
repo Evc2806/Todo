42a6ed884ab2190efe4db59589738f8236e05b4d
Evc2806 <86616101+Evc2806@users.noreply.github.com>
2025-03-25 10:07:47 -0500

    Update and rename Lista1.py to Mostrar _elemento_Lista.py

---
+++ A_TALENTO/Programacion_Explorador/Listas/Mostrar _elemento_Lista.py
@@ -0,0 +1,42 @@
+# Mostrar un elemento específico de la lista
+
+
+
+lista1 = [3, 6.8, "edwin", 67, 4]
+
+
+print(lista1[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#cambiar dato
+##numeros_enteros[1]=5
+
+#print(numeros_enteros[1])
