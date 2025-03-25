06ee9dd102fade70ab7ed81ed16cf55ad9d9f600
Evc2806 <86616101+Evc2806@users.noreply.github.com>
2025-03-25 10:12:48 -0500

    Update and rename MOstrar-un_el_anida.py to Mostrar_elementos_lista_anida.py

--- A_TALENTO/Programacion_Explorador/Listas/Mostrar_elementos_lista_anida.py
+++ A_TALENTO/Programacion_Explorador/Listas/Mostrar_elementos_lista_anida.py
@@ -1,8 +1,8 @@
-# Definir una lista de listas (lista anidada)
+# Definir una lista de listas y acceder a los ele,entos
 lista_anidada = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 # Acceder al número 5, que está en la segunda lista interna (índice 1) y en la segunda posición (índice 1)
-numero_5 = lista_anidada[2][2]
+numero_5 = lista_anidada[1][1]
 
 # Mostrar el número 5
 print(numero_5)
